3d695c51fe2a13b50ae964f37836e21ce387bdb5
CeliaTHP <Tibiza121>
2022-02-15 14:30:00 +0100

    - Add comments to explain bugs and fixes

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.java
@@ -14,31 +14,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //references a item from another layout not declared
-        //activity_second_activity_text_view_main is refered in the layout for the second activity
+        //crash n° 1 caused by : an item from another layout referenced here
+        //activity_second_activity_text_view_main only exists in the layout for the second activity
+        //fixed by : changing the text view reference according to the corresponding view
         this.textViewMain = findViewById(R.id.activity_main_activity_text_view_main);
         //ORIGINAL LINE
         //this.textViewMain = findViewById(R.id.activity_second_activity_text_view_main);
-
         this.textViewQuantity = findViewById(R.id.activity_main_activity_text_view_quantity);
 
         this.configureTextViewMain();
         this.configureTextViewQuantity();
     }
 
-    private void configureTextViewMain(){
-        //crash here
+    private void configureTextViewMain() {
+        //CRASH N°1
         this.textViewMain.setTextSize(15);
         this.textViewMain.setText("Le premier bien immobilier enregistré vaut ");
     }
 
-    private void configureTextViewQuantity(){
-        //also crashes here
+    private void configureTextViewQuantity() {
+
+        //crash n° 2 caused by a resource not found
+        //when using an int for a setText, it refers to resource id
+        //fixed by : converting our variable to string by adding ""
+        String quantity = Utils.convertDollarToEuro(100) + "";
         //ORIGINAL LINE
         //int quantity = Utils.convertDollarToEuro(100);
-        String quantity = Utils.convertDollarToEuro(100)+ "";
         this.textViewQuantity.setTextSize(20);
+        //CRASH N°2
         this.textViewQuantity.setText(quantity);
     }
 }
